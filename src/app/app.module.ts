ec6ca9e1314958f3d2ebda878a3bd622df2384bc
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-06-02 16:28:44 +0100

    bootstrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule
