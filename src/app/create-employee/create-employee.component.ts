332479c6c5c26ce807a4fd9e93327893bc79cb76
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-06-05 16:36:55 +0100

    nested form group added

--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-create-employee',
@@ -14,12 +14,18 @@ export class CreateEmployeeComponent implements OnInit {
 
   ngOnInit(): void {
     this.employeeForm = new FormGroup({
-      fullName: new FormControl(''),
-      email: new FormControl('')
+      fullName: new FormControl('', Validators.required),
+      email: new FormControl(''),
+      skills: new FormGroup({
+        skillName: new FormControl(''),
+        experienceInYears: new FormControl(''),
+        proficiency: new FormControl('')
+      })
     })
   }
 
   submit() {
-    console.log(this.employeeForm)
+    console.log(this.employeeForm);
+    this.employeeForm.reset()
   }
 }
